1ecca36b77b81518e4256a6934d9eda0bcef0165
istiyak <istiyaq.mi9@gmail.com>
2020-01-22 15:17:50 +0530

    added gitignore file

--- Server/update.bat
+++ Server/update.bat
@@ -1,12 +1,22 @@
 @echo off
+
 goto :main
 
 
+
+
 :main
+
 setlocal
+	
 	echo Downloading your files ...
-	git clone https://github.com/Istiyakmi9/ShopServer.git
+	
+	git clone https://github.com/Istiyakmi9/shop.git
+	
 	echo Downloaded successfully.
+	
 	pause
+
 endlocal
+
 goto :eof
